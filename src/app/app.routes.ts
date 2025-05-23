8586373b7f1677e3ddb60d4b09350c7c3803f792
sathishdass-1690 <sathishdass-1690>
2025-05-23 19:38:12 +0530

    Commit on error fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomelayoutComponent } from './Shared/homelayout/homelayout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path:'', component:HomelayoutComponent},
+];
