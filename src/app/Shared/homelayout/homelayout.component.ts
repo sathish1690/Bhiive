86c31f0c4dfe34e0068cf7bd40f81fe49ff9ad90
sathishdass-1690 <sathishdass-1690>
2025-07-18 17:10:13 +0530

    add blog component

--- src/app/Shared/homelayout/homelayout.component.ts
+++ src/app/Shared/homelayout/homelayout.component.ts
@@ -7,11 +7,12 @@ import { ResquestquoteComponent } from './resquestquote/resquestquote.component'
 import { ServiceComponent } from './service/service.component';
 import { ProtfolioComponent } from './protfolio/protfolio.component';
 import { PricingComponent } from './pricing/pricing.component';
+import { BlogComponent } from './blog/blog.component';
 
 @Component({
   selector: 'app-homelayout',
   standalone: true,
-  imports: [CarouselModule,AboutComponent, ServiceComponent, FeedbackComponent, ProjectsComponent, ResquestquoteComponent, ProtfolioComponent],
+  imports: [CarouselModule,AboutComponent, ServiceComponent, FeedbackComponent, ProjectsComponent, ResquestquoteComponent, ProtfolioComponent, BlogComponent],
   templateUrl: './homelayout.component.html',
   styleUrl: './homelayout.component.scss'
 })
